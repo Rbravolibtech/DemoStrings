93ac399197a8ab8a508bd5f04916212b05b87630
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-11 12:06:02 -0700

    worked with strings and how thing work on c#

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace DemoStrings;
+﻿using System;
 
-class Program
+namespace DemoStrings
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            var firstName = "Cena";
+            var lastName = "Jon";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("my name is{0} {1}", firstName, lastName);
+
+            var names = new string[3] { "Jon", "Cena", "Mary" };
+            var formattedNames = string.Join(",", names);
+
+            Console.WriteLine(formattedNames);
+
+            //YOU CAN DO LINE OF CODE BELLOW ANOTHER BETTER AND MORE CLEAN WAY OF DOING CODE ON EX 2
+
+            //var text = "Hi Jon\nLook into the following paths\nc:\\folder1\\folder2\\nc:\\folder3\\folder4";
+            //Console.WriteLine(text);
+
+            // EX 2 THIS IS A BETTER WAY MAKE SURE TO AT @ FROM THE BEGENNING 
+
+            var text = @"Jon Look into the following paths  
+                c:\folder1/folder2  
+                c:\folder3\folder4";
+            Console.WriteLine(text);
+            //String lastName = "Jon";
+            //string myName = "mosh";
+        }
     }
 }
-
